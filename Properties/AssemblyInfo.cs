718812393a1ab7a81c81fc771db3634bb20ed76a
DigitalzombieTLD <blackdevil32@gmx.net>
2023-09-27 16:33:20 +0200

    Update for 2.23

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("AudioCore")]
 [assembly: AssemblyCopyright("Digitalzombie")]
-[assembly: AssemblyVersion("1.4.1")]
-[assembly: AssemblyFileVersion("1.4.1")]
-[assembly: MelonInfo(typeof(AudioCoreMain), "AudioCore", "1.4.1", "Digitalzombie", null)]
+[assembly: AssemblyVersion("2.2.3")]
+[assembly: AssemblyFileVersion("2.2.3")]
+[assembly: MelonInfo(typeof(AudioCoreMain), "AudioCore", "2.2.3", "Digitalzombie", null)]
 [assembly: MelonGame("Hinterland", "TheLongDark")]
